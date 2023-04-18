f1e9c8fe4c9060776e37d872ea4a35f70669753a
Ernesto <jorge.cywdt@gmail.com>
2023-04-17 20:58:49 -0500

    Notas para entender mejor el codigo

--- DjangoAjaxCRUD/urls.py
+++ DjangoAjaxCRUD/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from CRUDAPP import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePage,name="home"),
-    path('/insert_student', views.InsertStudent,name="insert"),
-    path('/update_all', views.update_all,name="update_all"),
-    path('/delete_data', views.delete_data,name="delete_data"),
+    path(''               , views.HomePage     , name="home"),
+    path('insert_student/', views.InsertStudent, name="insert"),
+    path('update_all/'    , views.update_all   , name="update_all"),
+    path('delete_data/'   , views.delete_data  , name="delete_data"),
 ]
