b39ec81f032e51681efc76370a8a2ce7ecdb6b71
Ernesto <jorge.cywdt@gmail.com>
2023-04-17 20:58:49 -0500

    Notas para entender mejor el codigo

--- CRUDAPP/models.py
+++ CRUDAPP/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class StudentData(models.Model):
+
+class StudentData(models.Model):  # Modelo creado para ejecutar migraciones
+    # Definir los datos de la clase (campos de la tabla)
+    # Si no se especifica lo contrario, todos los campos seran requeridos por defecto, es decir NOT NULL
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
     email=models.CharField(max_length=255)
     gender=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
-
